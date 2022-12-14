41b74e90c9b1329abfab6a9ee4ddcd23cae477ad
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-14 15:46:29 -0300

    Init marca class

---
+++ dominio/Marca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+    }
+}
