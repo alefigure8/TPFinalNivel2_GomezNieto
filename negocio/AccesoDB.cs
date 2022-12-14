a2fbcfa5f310b25e672fcd495b4ab3709734723a
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-14 15:45:52 -0300

    Add Access to SQL Server

---
+++ negocio/AccesoDB.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace negocio
+{
+    internal class AccesoDB
+    {
+
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+        }
+
+        public AccesoDB()
+        {
+            string serverName = "DESKTOP-81ER52L";
+            string DbName = "CATALOGO_DB";
+            connection = new SqlConnection($"server={serverName}\\SQLEXPRESS; database={DbName}; integrated security=true");
+            command = new SqlCommand();
+        }
+
+        public void setQuery(string query)
+        {
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = query;
+        }
+
+        public void executeReader()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                reader = command.ExecuteReader();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void executeNonQuery()
+        {
+            command.Connection = connection;
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
