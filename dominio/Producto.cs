44eda102ce09b04d5ce58afba05612f2dc1a7293
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-14 15:46:37 -0300

    Init producto class

---
+++ dominio/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        [DisplayName("Código")]
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public string ImagenURL { get; set; }
+        [DisplayName("Marca")]
+        public Marca MarcaInfo { get; set; }
+        [DisplayName("Categoria")]
+        public Categoria CategoriaInfo{ get; set; }
+    }
+}
