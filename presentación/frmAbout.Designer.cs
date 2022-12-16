d324e70b2deb032e8c1f2edc40e1dfe4a4d398e0
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-16 14:44:17 -0300

    update and clean

---
+++ presentación/frmAbout.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace presentación
+{
+    partial class frmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFrmAboutVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbFrmAboutVersion
+            // 
+            this.lbFrmAboutVersion.AutoSize = true;
+            this.lbFrmAboutVersion.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFrmAboutVersion.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbFrmAboutVersion.Location = new System.Drawing.Point(58, 80);
+            this.lbFrmAboutVersion.Name = "lbFrmAboutVersion";
+            this.lbFrmAboutVersion.Size = new System.Drawing.Size(88, 14);
+            this.lbFrmAboutVersion.TabIndex = 0;
+            this.lbFrmAboutVersion.Text = "Version 0.0.1";
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(86)))), ((int)(((byte)(111)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbFrmAboutVersion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmAbout";
+            this.Text = "frmAbout";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbFrmAboutVersion;
+    }
+}
--- presentación/frmPrincipal.Designer.cs
+++ presentación/frmPrincipal.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace presentación
+﻿using System.Windows.Forms;
+
+namespace presentación
 {
     partial class frmPrincipal
     {
@@ -28,38 +30,266 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.labelFrmPrincipalCatalogo = new System.Windows.Forms.Label();
+            this.lbShow = new System.Windows.Forms.Label();
+            this.comboBoxMostrarCantidad = new System.Windows.Forms.ComboBox();
+            this.lbEncontrados = new System.Windows.Forms.Label();
+            this.comboBoxOrdenar = new System.Windows.Forms.ComboBox();
+            this.lbOrdenar = new System.Windows.Forms.Label();
+            this.lbPaginas = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.PictureBox();
+            this.btnLast = new System.Windows.Forms.PictureBox();
+            this.btnFirst = new System.Windows.Forms.PictureBox();
+            this.btnPreview = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLast)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPreview)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvProductos
             // 
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            this.dgvProductos.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProductos.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvProductos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dgvProductos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle7.NullValue = null;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.DarkOrange;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProductos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            resources.ApplyResources(this.dgvProductos, "dgvProductos");
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle8.NullValue = null;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvProductos.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dgvProductos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvProductos.GridColor = System.Drawing.SystemColors.WindowText;
+            this.dgvProductos.Location = new System.Drawing.Point(28, 118);
             this.dgvProductos.Name = "dgvProductos";
-            this.dgvProductos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgvProductos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProductos.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.dgvProductos.RowHeadersVisible = false;
+            this.dgvProductos.RowHeadersWidth = 35;
+            this.dgvProductos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvProductos.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            this.dgvProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvProductos.Size = new System.Drawing.Size(550, 252);
+            this.dgvProductos.TabIndex = 0;
+            // 
+            // labelFrmPrincipalCatalogo
+            // 
+            this.labelFrmPrincipalCatalogo.AutoSize = true;
+            this.labelFrmPrincipalCatalogo.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFrmPrincipalCatalogo.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labelFrmPrincipalCatalogo.Location = new System.Drawing.Point(33, 47);
+            this.labelFrmPrincipalCatalogo.Name = "labelFrmPrincipalCatalogo";
+            this.labelFrmPrincipalCatalogo.Size = new System.Drawing.Size(127, 25);
+            this.labelFrmPrincipalCatalogo.TabIndex = 1;
+            this.labelFrmPrincipalCatalogo.Text = "CATÁLOGO";
+            // 
+            // lbShow
+            // 
+            this.lbShow.AutoSize = true;
+            this.lbShow.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbShow.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbShow.Location = new System.Drawing.Point(429, 86);
+            this.lbShow.Name = "lbShow";
+            this.lbShow.Size = new System.Drawing.Size(59, 16);
+            this.lbShow.TabIndex = 2;
+            this.lbShow.Text = "Mostrar";
+            // 
+            // comboBoxMostrarCantidad
+            // 
+            this.comboBoxMostrarCantidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMostrarCantidad.FormattingEnabled = true;
+            this.comboBoxMostrarCantidad.Location = new System.Drawing.Point(494, 85);
+            this.comboBoxMostrarCantidad.Name = "comboBoxMostrarCantidad";
+            this.comboBoxMostrarCantidad.Size = new System.Drawing.Size(84, 21);
+            this.comboBoxMostrarCantidad.TabIndex = 3;
+            this.comboBoxMostrarCantidad.SelectedIndexChanged += new System.EventHandler(this.comboBoxMostrarCantidad_SelectedIndexChanged);
+            // 
+            // lbEncontrados
+            // 
+            this.lbEncontrados.AutoSize = true;
+            this.lbEncontrados.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEncontrados.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbEncontrados.Location = new System.Drawing.Point(35, 87);
+            this.lbEncontrados.Name = "lbEncontrados";
+            this.lbEncontrados.Size = new System.Drawing.Size(171, 16);
+            this.lbEncontrados.TabIndex = 4;
+            this.lbEncontrados.Text = "5 productos encontrados";
+            // 
+            // comboBoxOrdenar
+            // 
+            this.comboBoxOrdenar.DropDownHeight = 80;
+            this.comboBoxOrdenar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOrdenar.FormattingEnabled = true;
+            this.comboBoxOrdenar.IntegralHeight = false;
+            this.comboBoxOrdenar.Location = new System.Drawing.Point(339, 86);
+            this.comboBoxOrdenar.Name = "comboBoxOrdenar";
+            this.comboBoxOrdenar.Size = new System.Drawing.Size(84, 21);
+            this.comboBoxOrdenar.TabIndex = 6;
+            this.comboBoxOrdenar.SelectedIndexChanged += new System.EventHandler(this.comboBoxOrdenar_SelectedIndexChanged);
+            // 
+            // lbOrdenar
+            // 
+            this.lbOrdenar.AutoSize = true;
+            this.lbOrdenar.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOrdenar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbOrdenar.Location = new System.Drawing.Point(247, 87);
+            this.lbOrdenar.Name = "lbOrdenar";
+            this.lbOrdenar.Size = new System.Drawing.Size(86, 16);
+            this.lbOrdenar.TabIndex = 5;
+            this.lbOrdenar.Text = "Ordenar por";
+            // 
+            // lbPaginas
+            // 
+            this.lbPaginas.AutoSize = true;
+            this.lbPaginas.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPaginas.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbPaginas.Location = new System.Drawing.Point(276, 390);
+            this.lbPaginas.Name = "lbPaginas";
+            this.lbPaginas.Size = new System.Drawing.Size(57, 16);
+            this.lbPaginas.TabIndex = 7;
+            this.lbPaginas.Text = "Páginas";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNext.Image = ((System.Drawing.Image)(resources.GetObject("btnNext.Image")));
+            this.btnNext.Location = new System.Drawing.Point(347, 390);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(11, 16);
+            this.btnNext.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnNext.TabIndex = 8;
+            this.btnNext.TabStop = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLast.Image = ((System.Drawing.Image)(resources.GetObject("btnLast.Image")));
+            this.btnLast.Location = new System.Drawing.Point(364, 390);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(18, 16);
+            this.btnLast.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnLast.TabIndex = 9;
+            this.btnLast.TabStop = false;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFirst.Image = ((System.Drawing.Image)(resources.GetObject("btnFirst.Image")));
+            this.btnFirst.Location = new System.Drawing.Point(223, 390);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(18, 16);
+            this.btnFirst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnFirst.TabIndex = 11;
+            this.btnFirst.TabStop = false;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPreview.Image = ((System.Drawing.Image)(resources.GetObject("btnPreview.Image")));
+            this.btnPreview.Location = new System.Drawing.Point(247, 390);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(11, 17);
+            this.btnPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnPreview.TabIndex = 10;
+            this.btnPreview.TabStop = false;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
             // 
             // frmPrincipal
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(86)))), ((int)(((byte)(111)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1445, 715);
             this.ControlBox = false;
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lbPaginas);
+            this.Controls.Add(this.comboBoxOrdenar);
+            this.Controls.Add(this.lbOrdenar);
+            this.Controls.Add(this.lbEncontrados);
+            this.Controls.Add(this.comboBoxMostrarCantidad);
+            this.Controls.Add(this.lbShow);
+            this.Controls.Add(this.labelFrmPrincipalCatalogo);
             this.Controls.Add(this.dgvProductos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MinimizeBox = false;
             this.Name = "frmPrincipal";
             this.ShowIcon = false;
-            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Principal";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLast)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPreview)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Label labelFrmPrincipalCatalogo;
+        private Label lbShow;
+        private ComboBox comboBoxMostrarCantidad;
+        private Label lbEncontrados;
+        private ComboBox comboBoxOrdenar;
+        private Label lbOrdenar;
+        private Label lbPaginas;
+        private PictureBox btnNext;
+        private PictureBox btnLast;
+        private PictureBox btnFirst;
+        private PictureBox btnPreview;
     }
 }
