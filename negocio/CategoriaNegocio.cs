61418106cdcffeb5e5e83b6fd6813ae8f58eddea
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-16 20:47:16 -0300

    Listar categorias desde la base de datos

---
+++ negocio/CategoriaNegocio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listaCategoria = new List<Categoria>();
+        public List<Categoria> listar()
+        {
+            AccesoDB datoSQL = new AccesoDB();
+
+            try
+            {
+                datoSQL.setQuery("SELECT * FROM CATEGORIAS");
+                datoSQL.executeReader();
+
+                while(datoSQL.Reader.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datoSQL.Reader["Id"];
+                    aux.Descripcion = (string)datoSQL.Reader["Descripcion"];
+
+                    listaCategoria.Add(aux);
+
+                }
+
+                return listaCategoria;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datoSQL.closeConnection();
+            }
+        }
+    }
+}
