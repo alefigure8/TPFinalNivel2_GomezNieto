fa5d58969cad4d7be4eccdc0b3a28639a2852b49
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-21 07:30:47 -0300

    Init class con carga de combo box

---
+++ helper/ComboBoxOptions.cs
@@ -0,0 +1,85 @@
+﻿using configuracion;
+using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static configuracion.Opciones;
+
+namespace helper
+{
+    public static class ComboBoxOptions
+    {
+        public static void comboBoxMarca(ComboBox combo)
+        {
+            try
+            {
+                listaDesplegable listaMarca = new listaDesplegable();
+                combo.DataSource = listaMarca.cargarMarcas();
+                combo.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Opciones.MensajeError.LISTAERROR);
+            }
+        }
+        public static void comboBoxCategoria(ComboBox combo)
+        {
+            try
+            {
+                listaDesplegable listaCategoria = new listaDesplegable();
+                combo.DataSource = listaCategoria.cargarCategorias();
+                combo.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Opciones.MensajeError.LISTAERROR);
+            }
+        }
+
+        public static void comboBoxCamposBusquedaAvanzada(ComboBox combo)
+        {
+            try
+            {
+                listaDesplegable listaCampos = new listaDesplegable();
+                combo.DataSource = listaCampos.cargarBusqueraColumnas();
+                combo.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Opciones.MensajeError.LISTAERROR);
+            }
+        }
+
+        public static void comboBoxOrdenarPor(ComboBox combo)
+        {
+            try
+            {
+                listaDesplegable listaCantidad = new listaDesplegable();
+                combo.DataSource = listaCantidad.cargarBusqueraColumnas();
+                combo.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Opciones.MensajeError.LISTAERROR);
+            }
+        }
+
+        public static void comboBoxCantidad(ComboBox combo)
+        {
+            try
+            {
+                listaDesplegable listaCantidad = new listaDesplegable();
+                combo.DataSource = listaCantidad.cargarBusquedaNumeros();
+                combo.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(Opciones.MensajeError.LISTAERROR);
+            }
+        }
+    }
+
+}
