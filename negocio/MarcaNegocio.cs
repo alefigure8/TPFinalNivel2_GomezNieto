5030cca2e54bbdcddd0750f78635fa034e825f94
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-21 07:31:40 -0300

    add Método agregar

--- negocio/MarcaNegocio.cs
+++ negocio/MarcaNegocio.cs
@@ -1,6 +1,8 @@
-﻿using dominio;
+﻿using configuracion;
+using dominio;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +35,83 @@ namespace negocio
             }
             catch (Exception ex)
             {
+                throw ex;
+            }
+            finally
+            {
+                datoSQL.closeConnection();
+            }
+        }
+
+        public List<Marca> buscar(string filtro)
+        {
+            AccesoDB datoSQL = new AccesoDB();
+            List<Marca> listaMarca = new List<Marca>();
+
+            try
+            {
+                datoSQL.setQuery($"select * from {Opciones.DBTablas.MARCAS} where descripcion like '%{filtro}%'");
+                datoSQL.executeReader();
+                while (datoSQL.Reader.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datoSQL.Reader[Opciones.Campo.ID];
+                    aux.Descripcion = (string)datoSQL.Reader[Opciones.Campo.DESCRIPCION];
+
+                    listaMarca.Add(aux);
+                }
+                return listaMarca;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            { 
+                datoSQL.closeConnection();
+            }
+        }
+
+        public bool agregar(string keyword)
+        {
+            AccesoDB datoSQL = new AccesoDB();
+
+            try
+            {
+                datoSQL.setQuery($"INSERT INTO {Opciones.DBTablas.MARCAS} ({Opciones.Campo.DESCRIPCION}) VALUES ('{keyword}')");
+                if (datoSQL.executeNonQuery())
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datoSQL.closeConnection();
+            }
+
+            return false;
+        }
+
+        public bool existeMarca(string keyword)
+        {
+            AccesoDB datoSQL = new AccesoDB();
+
+            try
+            {
+                datoSQL.setQuery($"SELECT * FROM {Opciones.DBTablas.MARCAS} WHERE {Opciones.Campo.DESCRIPCION} = '{keyword}'");
+                datoSQL.executeReader();
+
+                if(datoSQL.Reader.Read())
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+
                 throw ex;
             }
             finally
