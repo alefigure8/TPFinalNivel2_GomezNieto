8af37b468352ae23f6c4a46a3b4d66e264a481ce
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-21 07:31:30 -0300

    Add método agregar

--- negocio/CategoriaNegocio.cs
+++ negocio/CategoriaNegocio.cs
@@ -41,5 +41,52 @@ namespace negocio
                 datoSQL.closeConnection();
             }
         }
+
+        public bool agregar(string keyword)
+        {
+            AccesoDB datoSQL = new AccesoDB();
+
+            try
+            {
+                datoSQL.setQuery($"INSERT INTO {Opciones.DBTablas.CATEGORIAS} ({Opciones.Campo.DESCRIPCION}) VALUES ('{keyword}')");
+                if (datoSQL.executeNonQuery())
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datoSQL.closeConnection();
+            }
+
+            return false;
+        }
+
+        public bool existeCategoria(string keyword)
+        {
+            AccesoDB datoSQL = new AccesoDB();
+
+            try
+            {
+                datoSQL.setQuery($"SELECT * FROM {Opciones.DBTablas.CATEGORIAS} WHERE {Opciones.Campo.DESCRIPCION} = '{keyword}'");
+                datoSQL.executeReader();
+
+                if (datoSQL.Reader.Read())
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datoSQL.closeConnection();
+            }
+        }
     }
 }
