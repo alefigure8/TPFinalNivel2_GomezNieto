5410abe6823af50d952e14dba5f66ab69322eb29
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-21 19:29:42 -0300

    init validacion

---
+++ helper/Validacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace helper
+{
+    public static class Validacion
+    {
+        public static bool esNumero(string numero)
+        {
+            decimal validar;
+            if(decimal.TryParse(numero, out validar))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool estaVacio(List<string> list)
+        {
+            if(list.Contains("") && list.Contains(null))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool estaSeleccionado(ComboBox combo)
+        {
+            if (combo.SelectedIndex < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
