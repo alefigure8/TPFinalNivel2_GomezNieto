d105a3feb0daee8d8057355f21d23734d403c6e4
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-27 18:20:19 -0300

    Adding cantidad a atributos

---
+++ dominio/Presupuesto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Presupuesto : Producto
+    {
+        public int cantidad { get; set; }
+        public decimal total { get; set; }
+    }
+}
