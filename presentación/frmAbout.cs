d4a2e0b3d7429a372c105c7824454b353793bf29
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2022-12-30 14:44:04 -0300

    Adding créditos, mail, página y github

--- presentación/frmAbout.cs
+++ presentación/frmAbout.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using configuracion;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +19,52 @@ namespace presentación
         public frmAbout()
         {
             InitializeComponent();
+        }
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Directory.GetCurrentDirectory().Replace(@"\bin", "")) + Opciones.Folder.ROOTIMAGE;
+            pbVersion.Load(path + Opciones.Folder.VERSION);
+            pbAutor.Load(path + Opciones.Folder.AUTOR);
+            pbGitHub.Load(path + Opciones.Folder.GITHUB);
+            pbWeb.Load(path + Opciones.Folder.WEB);
+            pbMail.Load(path + Opciones.Folder.MAIL);
+        }
+
+        private void lbGithHub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://github.com/alefigure8/TPFinalNivel2_GomezNieto");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se puede acceder al link");
+            }
+        }
+
+        private void lbWeb_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://alegomeznieto.tech");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se puede acceder al link");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                Process.Start("mailto:gomeznieto@gmail.com");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se puede acceder al link");
+            }
         }
     }
 }
